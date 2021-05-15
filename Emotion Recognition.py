bf7ac9e3430cf4ac94a338e3cb97ee438abadcb2
Mani Singh <75898710+mani24singh@users.noreply.github.com>
2021-05-16 02:15:46 +0530

    application
    Emotion recognition application design using emotion recognition library and pytorch

---
+++ Emotion Recognition.py
@@ -0,0 +1,19 @@
+from facial_emotion_recognition import EmotionRecognition
+
+import cv2 as cv
+
+er = EmotionRecognition(device='cpu')
+
+cam = cv.VideoCapture(0)
+
+while True:
+    success, frame = cam.read()
+    frame = er.rcognise_emotion(frame, return_type='BGR')
+    cv.imshow('frame',frme)
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+
+cam.release()
+cv2.destrroyAllWindows()
+
